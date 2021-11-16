3e963623b689df320c05e505c5170c830072b8cb
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-11-15 21:13:11 -0800

    change print string

--- functions/print.js
+++ functions/print.js
@@ -4,6 +4,6 @@ function print(text) {
   console.log('------------')
 }
 
-print('My name is Santi')
-print('I a starting launch school agian')
-print('I want to be a world class software engineer')
+print('My name is Santi');
+print('I a starting launch school agian');
+print('My goal is to become a software engineer');
